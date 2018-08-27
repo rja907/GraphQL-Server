70385fe3a993dc17a2970942bfcccdc12030ba2a
Raj <kmrrajj@gmail.com>
2018-08-26 22:50:16 -0700

    bug fixes

--- schema.js
+++ schema.js
@@ -28,15 +28,17 @@ const CustomerType = new GraphQLObjectType({
 // Root Query
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
-  customer: {
-    type: CustomerType,
-    args: {
-      id: { GraphQLString }
-    },
-    resolve(parentValue, args) {
-      for (let i = 0; i < customers.length; i++) {
-        if (customers[i].id === args.id) {
-          return customers[i];
+  fields: {
+    customer: {
+      type: CustomerType,
+      args: {
+        id: { type: GraphQLString }
+      },
+      resolve(parentValue, args) {
+        for (let i = 0; i < customers.length; i++) {
+          if (customers[i].id === args.id) {
+            return customers[i];
+          }
         }
       }
     }
